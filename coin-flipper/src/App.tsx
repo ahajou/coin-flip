f1c9e006e174d6f8c032f2f6357cf214e110978e
Ali Karaki <ali.m.karaki@gmail.com>
2022-04-19 20:47:41 +0300

    change to web3-react wip

--- coin-flipper/src/App.tsx
+++ coin-flipper/src/App.tsx
@@ -1,115 +1,17 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Web3 from 'web3';
-import contract from './CoinFlip.json';
-import { Contract } from 'web3-eth-contract';
-import { Game } from './Models/Game';
-import { Connected } from './Components/Connected';
+import "./App.css";
+import HomePage from "./HomePage";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const CONTRACT_ABI:any = contract.abi;
+function getLibrary(provider: any) {
+  return new Web3(provider);
+}
 
 function App() {
-  const [account, setAccount] = useState<string>('');
-  const [balance, setBalance] = useState<number>(0);
-
-  const [web3, setWeb3] = useState<Web3>();
-  const [contract, setContract] = useState<Contract>();
-  const [games, setGames] = useState<Game[]>([]);
-  
-  const refreshGames = async (web3: Web3, cc: Contract) => {
-    const newGamesList:Game[] = [];
-
-    const gameCount = await cc.methods.getGameCount().call();
-    for (var i = gameCount - 1; i >= 0; i--) {
-      const game = await cc.methods.getGameEntry(i).call();
-      newGamesList.push({
-        addr: game.addr,
-        blocknumber: game.blocknumber,
-        blocktimestamp: game.blocktimestamp,
-        bet: +web3.utils.fromWei(game.bet, 'ether'),
-        prize: +web3.utils.fromWei(game.prize, 'ether'),
-        winner: game.winner
-      });
-    }
-
-    setGames(newGamesList);
-  }
-
-  const refreshBalance = async (web3: Web3, account: string) => {
-    const bb = await web3.eth.getBalance(account);
-    setBalance(+web3.utils.fromWei(bb, 'ether'));
-  }
-
-  const initConnection = async (web3: Web3, account: string) => {
-    setAccount(account);
-
-    const gameContract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
-    setContract(gameContract);
-
-    await refreshBalance(web3, account);
-    await refreshGames(web3, gameContract);
-  };
-
-  const connect = async () => {
-    if(!web3) {
-      return;
-    }
-
-    const accounts = await web3?.eth.requestAccounts();
-    if(!accounts) {
-      return;
-    }
-
-    await initConnection(web3, accounts[0]);
-  }
-
-  useEffect(() => {
-    const checkConnection = async function () {
-      const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-      setWeb3(web3);
-      
-      try {
-        const accounts = await web3.eth.getAccounts();
-        await initConnection(web3, accounts[0]);
-      }
-      catch {
-
-      }
-    }
-
-    checkConnection();
-  }, []);
-
-  const bet = async (amountToBet: number) => {
-    if(!contract || !web3 || !account) {
-      return;
-    }
-
-    await contract.methods.play().send({from: account, value: web3.utils.toWei(amountToBet.toString(), 'ether')});
-    // console.log(response);
-
-    await refreshGames(web3, contract);
-    await refreshBalance(web3, account);
-  }
-
   return (
-    <div className='flex flex-col items-center'>
-      <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-blue-600">Coin Flipper</h1>
-      {account 
-        ? <Connected games={games} account={account} balance={balance} placeBet={bet} />
-        : 
-          <div>
-            <div className="bg-yellow-100 rounded-lg py-5 px-6 mb-3 text-base text-yellow-700 inline-flex items-center w-full" role="alert">
-              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-triangle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                <path fill="currentColor" d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path>
-              </svg>
-              No account connected.
-            </div>
-            <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={connect}>Connect</button>
-          </div>
-      }
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <HomePage />
+    </Web3ReactProvider>
   );
 }
 
